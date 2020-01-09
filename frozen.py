afab4f777b5cc98d86c1f156bceb66cfcb47020d
Francis Z. Fang <777+Franc1sFrank@users.noreply.github.com>
2020-01-08 19:23:27 -0500

    parameters

--- frozen.py
+++ frozen.py
@@ -1,1 +1,9 @@
 import taichi as ti
+quality = 1 # Use a larger value for higher-res simulations
+n_particles, n_grid = 9000 * quality ** 2, 128 * quality
+dx, inv_dx = 1 / n_grid, float(n_grid)
+dt = 1e-4 / quality
+p_vol, p_rho = (dx * 0.5)**2, 1
+p_mass = p_vol * p_rho
+E, nu = 0.1e4, 0.2 # Young's modulus and Poisson's ratio
+mu_0, lambda_0 = E / (2 * (1 + nu)), E * nu / ((1+nu) * (1 - 2 * nu)) # Lame parameters
